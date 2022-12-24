ef331b94f3853b2b95c2edaa9736addc80a500f5
Anwyll Le <lelinhnguyen2109@gmail.com>
2022-12-24 04:02:04 -0500

    continue

--- src/Components/UI/CustomCard.jsx
+++ src/Components/UI/CustomCard.jsx
@@ -1,22 +1,30 @@
-import React from 'react'
-import { Container, Card, Button } from 'react-bootstrap'
-import styles from "./CustomCard.module.css"
+import React from "react";
+import { Container, Card, Button } from "react-bootstrap";
+import styles from "./CustomCard.module.css";
 function CustomCard(props) {
+  let price = `$${(props.data.pricePerServing / 10).toFixed(2)}`
   return (
-    <Card className='col-3'>
-        <Card.Img variant="top" src={props.data.image} />
-        <Card.Body>
-            <Card.Title>{props.data.title}</Card.Title>
-            
-            {/* {data.id && <Card.Text>{parse(data.summary)}</Card.Text>} */}
-            <Container fluid className='d-flex justify-content-between align-items-center' >
-              <Button className={styles.button}>Add to cart</Button>
-              <Card.Text className={`${styles.price} d-inline-block fs-5 fw-bold fst-italic`}>${props.data.pricePerServing}</Card.Text>
-            </Container>
-            
-        </Card.Body>
+    <Card className="col-3">
+      <Card.Img variant="top" src={props.data.image} />
+      <Card.Body>
+        <Card.Title>{props.data.title}</Card.Title>
+
+        {/* {data.id && <Card.Text>{parse(data.summary)}</Card.Text>} */}
+        <Container
+          fluid
+          className="d-flex justify-content-between align-items-center"
+        >
+          <Button className={styles.button}>Add to cart</Button>
+          <Card.Text
+            className={`${styles.price} d-inline-block 
+              fs-5 fw-bold fst-italic`}
+          >
+            {price}
+          </Card.Text>
+        </Container>
+      </Card.Body>
     </Card>
-  )
+  );
 }
 
-export default CustomCard
+export default CustomCard;
