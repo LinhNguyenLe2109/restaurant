58c297a328905dfe0ecf26c3543f750581bbe20a
Anwyll Le <lelinhnguyen2109@gmail.com>
2023-03-03 19:59:33 -0500

    basic modals

--- src/Components/Body/Cart.jsx
+++ src/Components/Body/Cart.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import ReactDOM from "react-dom";
+import classes from "./Cart.module.css";
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.onClose}></div>;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const portalElement = document.getElementById("modal-section");
 
 function Cart(props) {
   return (
-    <div onClick={props.closeCart} >Cart here</div>
-  )
+    <React.Fragment>
+      {ReactDOM.createPortal(
+        <Backdrop onClose={props.closeCart} />,
+        portalElement
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay>
+          <div onClick={props.closeCart}>Cart here</div>
+        </ModalOverlay>,
+        portalElement
+      )}
+    </React.Fragment>
+  );
 }
 
-export default Cart
+export default Cart;
