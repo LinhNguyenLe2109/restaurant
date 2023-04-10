356bc2b10c569aeac68179dc37b8ebd0c6193d1b
Anwyll Le <lelinhnguyen2109@gmail.com>
2023-04-10 16:20:02 -0400

    Need to update the styling

--- components/CuisineList.js
+++ components/CuisineList.js
@@ -11,26 +11,28 @@ import "swiper/css/pagination";
 import "swiper/css/navigation";
 
 function CuisineList() {
-  let list = Array(10).fill(1).map((abc, idx) => (
-    <SwiperSlide key={idx}>
-      <Card style={{"color": "black"}}>
-        <Card.Img
-          variant="top"
-          src="https://www.ford.ca/cmslibs/content/dam/brand_ford/en_ca/brand/homepage/brand_gallery/2022/ford_racing_crop_960x540.jpg/_jcr_content/renditions/cq5dam.web.768.768.jpeg"
-        />
-        <Card.Body>
-          <Card.Title>{idx}</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up
-            the bulk of the card content.
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card>
-    </SwiperSlide>
-  ));
+  let list = Array(10)
+    .fill(1)
+    .map((abc, idx) => (
+      <SwiperSlide key={idx}>
+        <Card style={{ color: "black" }}>
+          <Card.Img
+            variant="top"
+            src="https://www.ford.ca/cmslibs/content/dam/brand_ford/en_ca/brand/homepage/brand_gallery/2022/ford_racing_crop_960x540.jpg/_jcr_content/renditions/cq5dam.web.768.768.jpeg"
+          />
+          <Card.Body>
+            <Card.Title>{idx}</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+      </SwiperSlide>
+    ));
   return (
-    <Container className={`${styles.container}`} fluid>
+    <Container className={`${styles.container} mb-5`} fluid>
       <Swiper
         slidesPerView={3}
         spaceBetween={30}
